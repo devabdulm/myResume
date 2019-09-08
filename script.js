d39ad6855a832319d43d95e976c5d561db560e2b
Abdulmuktadir <devabdulm@gmail.com>
2019-09-08 13:42:29 +0100

    Inproved the contact form, added more javascript for validation

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-let mailVer = document.querySelector(.email);
+
+const validateForm = () => {
+    let name = document.forms["contForm"]["Name"];
+    let title = document.forms["contForm"]["Title"];
+    let email = document.forms["contForm"]["Email"];
+    let message = document.forms["contForm"]["Message"];
+
+    if (name.value == "" || name.value.length < 4 ){
+        window.alert("Please enter atleast 4 letter name!");
+        name.focus();
+        return false;
+    } 
+
+    if (email.value == ""){
+        window.alert("Enter email please!");
+        name.focus();
+        return false;
+    }
+    if (title.value == ""){
+        window.alert("Enter title please!");
+        name.focus();
+        return false;
+    }
+    if (message.value == "" || message.value.length < 19){
+        window.alert("Enter a 20 character message please!");
+        name.focus();
+        return false;
+    }
+};
+
+
+
+
